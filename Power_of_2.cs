8fa76719f5dbbc65ee9121b06319e25850a960f3
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-29 15:06:19 +0530

    Added:Power_of_2.cs file

---
+++ Power_of_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Basic_Programs
+{
+    internal class Power_of_2
+    {
+        public static void Main()
+        {
+            Console.WriteLine("enter no. as power of 2");
+
+            int n =int.Parse(Console.ReadLine());
+            int number = 2;
+            int sum = 1;
+            for(int i=0;i<n;i++)
+            {
+                sum = sum * number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
