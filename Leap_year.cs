f04dac85fafb230ecfcc7fd1d3b97c270ced5154
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-29 14:49:27 +0530

    Added:Leap_year.cs file

---
+++ Leap_year.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Basic_Programs
+{
+    internal class Leap_year
+    {
+        
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("enter a year ");
+            int year = int.Parse(Console.ReadLine());
+
+            if (year % 4 == 0 )
+            {
+                Console.WriteLine(year+" is leap year");
+
+
+            }
+            
+            else if(year%400==0) 
+            {
+                Console.WriteLine(year+" is leap year");
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine(year+" is not leap year");
+            }
+            else
+            {
+                {
+                    Console.WriteLine(year + " is not leap year");
+                }
+            }
+        }
+    }
+}
