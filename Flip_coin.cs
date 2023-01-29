d192bfaef323308dfb2db60a19deb94a9e560940
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-01-29 14:03:26 +0530

    modified :Flip_coin.cs file

--- Flip_coin.cs
+++ Flip_coin.cs
@@ -13,33 +13,33 @@ namespace Basic_Programs
             Random rnd = new Random();
             int headcount = 0;
             int tailcount = 0;
-            if (tosCount/2==0) 
+            if (tosCount % 2 == 0)
             {
-                   for (int i = 0; i <= tosCount; i++)
-                   {
-                        int tos = rnd.Next(0, 2);
-                     if (tos == 0)
-                     {
+                for (int i = 0; i <= tosCount; i++)
+                {
+                    int tos = rnd.Next(0, 2);
+                    if (tos == 0 && tos <= 20)
+                    {
                         Console.WriteLine("Head");
 
-                          headcount++;
-                     }
+                        headcount++;
+                    }
 
-                     else
-                     {
-                  
-                       Console.WriteLine("Tail");
-                       tailcount++;
-                   
-                     }
-                   }
+                    else
+                    {
+
+                        Console.WriteLine("Tail");
+                        tailcount++;
+
+                    }
+                }
 
                 Console.WriteLine("Head count " + headcount);
                 Console.WriteLine("Tail count " + tailcount);
-              
-                
-                
-                
+
+
+
+
                 if (headcount > tailcount)
                 {
                     Console.WriteLine("Head count are more Than Tail count \n \n    **winer is Head**  ");
@@ -50,11 +50,14 @@ namespace Basic_Programs
 
                 }
             }
- 
-            else 
+            else if (tosCount>20)
+            {
+                Console.WriteLine("you can enter only maximum 20");           
+            }
+            else
             {
                 Console.WriteLine("you enterd a odd no. please enter a even no.");
-                    
+
             }
         }
     }
